2e093db75a229a6b35b4e9d67a17aba576b52195
RaghavPal <raghav.pal.tx@gmail.com>
2017-09-14 20:15:26 +0530

    Signed-off-by: RaghavPal <raghav.pal.tx@gmail.com>

--- src/test/java/com/tx/dev/DevProject/Test1.java
+++ src/test/java/com/tx/dev/DevProject/Test1.java
@@ -3,7 +3,7 @@ package com.tx.dev.DevProject;
 public class Test1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Inside Dev Test ");
+		System.out.println("Hello World ..");
 	}
 
 }
