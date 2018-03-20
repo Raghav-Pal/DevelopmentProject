da87a34a9d5341c41569d571d0441890a2122150
Raghav-Pal <raghav.pal@enernoc.com>
2018-03-20 15:34:44 +0530

    Signed-off-by: Raghav-Pal <raghav.pal@enernoc.com>

--- src/main/java/com/tx/dev/DevProject/DevTest1.java
+++
@@ -1,9 +0,0 @@
-package com.tx.dev.DevProject;
-
-public class DevTest1 {
-	
-	public static void main(String[] args) {
-		System.out.println("Inside Dev Test");
-	}
-
-}
--- src/test/java/com/tx/dev/DevProject/FactorialTest.java
+++
@@ -1,7 +0,0 @@
-package com.tx.dev.DevProject;
-
-import junit.framework.TestCase;
-
-public class FactorialTest extends TestCase {
-
-}
